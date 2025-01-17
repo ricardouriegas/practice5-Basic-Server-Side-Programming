bfc2cb816fdc09eeb7be8ac716bec446154ae720
Ricardo Uriegas <risardo01@gmail.com>
2025-01-17 17:57:24 -0600

    config correction

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 
 // Root de la aplicación a partir de http://localhost
-define("APP_ROOT", "/practica05/");
+define("APP_ROOT", "/practice5-Basic-Server-Side-Programming/");
 
 // Ruta física de la aplicación
-define("APP_PATH", "/opt/lampp/htdocs/practica05/");
+define("APP_PATH", "/opt/lampp/htdocs/practice5-Basic-Server-Side-Programming/");
 
 // Directorio donde se van a subir los archivos
-define("DIR_UPLOAD", "/opt/lampp/htdocs/practica05/uploads/");
+define("DIR_UPLOAD", "/opt/lampp/htdocs/practice5-Basic-Server-Side-Programming/uploads/");
 
 // Configuraciones de la base de datos a usar
 define("DB_DSN", "mysql:host=127.0.0.1;port=3306;dbname=file_manager_system;charset=utf8mb4;");
